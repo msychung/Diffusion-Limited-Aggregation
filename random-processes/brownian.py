988598552b63f5db11c5a9a93b67ad6cfefd09c4
Melissa Chung <melissachung888@gmail.com>
2021-02-16 18:07:45 +0000

    Added 2D Brownian motion

--- random-processes/brownian.py
+++ random-processes/brownian.py
@@ -14,6 +14,7 @@ class BrownianMotion():
     def __init__(self):
         pass
 
+
     def line(self):
         '''
         Produces a scatter plot of x co-ordinates against y co-ordinates, following a y = x line with minor deviations. 
@@ -24,10 +25,13 @@ class BrownianMotion():
         N = 100     # number of steps
         h = math.sqrt(T/N)
 
+        '''For larger N, it is more efficient to append to lists then call the dataframe constructor, than to create an empty dataframe and append to it.
+        This is because pandas append() returns a copy of the original df and the new one (quadratic copy with complexity O(N^2))'''
         x_list = []
         y_list = []
 
         for i in range(1, N + 1):
+            '''random() generates a random number from the uniform distribution'''
             x = i + h * random()
             y = i + h * random()
 
@@ -42,7 +46,8 @@ class BrownianMotion():
         ax.set(xlabel='x', ylabel='y', title='x-y plot')
         plt.show()
 
-    def brownian_motion(self):
+
+    def brownian_motion_1D(self):
         '''
         1D Brownian Motion Path
         '''
@@ -70,11 +75,56 @@ class BrownianMotion():
         del t, x, dx
 
         fig, ax = plt.figure(), plt.axes()
-        # ax.plot(df['x'], df['dx'], marker='o', markersize=1, linewidth=0)
-        # ax.set(xlabel='x', ylabel='dx', title='')
         ax.plot(df['t'], df['x'], marker='o', markersize=1, linewidth=0)
         ax.set(xlabel='Time t', ylabel='Random Variable $X(t)$', title='1D Brownian Motion Path')
         plt.show()  
 
+
+    def brownian_motion_2D(self):
+        '''
+        2D Brownian Motion Path
+        '''
+        # Initialise parameters
+        T = 1.0    # time step
+        N = 10000     # number of steps
+        dt = math.sqrt(T/(N-1))    # sqrt of time interval
+
+        '''For larger N, it is more efficient to append to lists then call the dataframe constructor, than to create an empty dataframe and append to it.
+        This is because pandas append() returns a copy of the original df and the new one (quadratic copy with complexity O(N^2))'''
+
+        t = np.linspace(0, T, N)
+
+        x = [0] * N    # Random variable x
+        dx = [0] * N     # Increment dx
+
+        dx[0] = dt * np.random.randn()
+        x[0] = dx[0]
+
+        y = [0] * N    # Random variable y
+        dy = [0] * N    # Increment dx
+
+        dy[0] = dt * np.random.randn()
+        y[0] = dy[0]
+
+        for i in range (1, N):
+            '''np.random.randn() generates a random number from the standard normal distribution'''
+            dx[i] = dt * np.random.normal()
+            x[i] = x[i-1] + dx[i] 
+
+            dy[i] = dt * np.random.normal()
+            y[i] = y[i-1] + dy[i] 
+
+        df = pd.DataFrame({'t': t, 'x': x, 'dx': dx, 'y': y, 'dy': dy})
+        del t, x, dx, y, dy
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot(df['t'], df['x'], df['y'], marker='o', markersize=1, linewidth=0)
+        ax.set_xlabel('Time t')
+        ax.set_ylabel('Random Variable $X(t)$')
+        ax.set_zlabel('Random Variable $Y(t)$')
+        ax.title.set_text('2D Brownian Motion')
+        plt.show()  
+
 test = BrownianMotion()
-test.brownian_motion()
+test.brownian_motion_2D()
