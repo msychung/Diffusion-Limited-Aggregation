456ee91324be026b05bfeda6855edc131acb46a3
Melissa Chung <melissachung888@gmail.com>
2021-03-02 14:46:22 +0000

    Combined animations into one mega method

--- random-processes/brownian_animation.py
+++ random-processes/brownian_animation.py
@@ -37,7 +37,8 @@ class BrownianAnimation():
 
     def generate_random_walks(self, dt, N, dimension):
         '''
-        Create a random walk line using scalar Brownian motion equations (as in brownian_prototype.py)
+        Create a random walk line using scalar Brownian motion equations (as in brownian_prototype.py).
+        Note that there is variable step size due to using np.random.randn(1,N), a standard normal distribution.
 
         Parameters
         ----------
@@ -48,7 +49,7 @@ class BrownianAnimation():
         dimension : str
             Selects which array is returned based on number of random variables
         '''
-        pos = np.linspace(0, 1000, 1001)
+        pos = np.linspace(0, 1000, 1001)    # no. of steps, for 1D plot
 
         dx = np.sqrt(dt) * np.random.randn(1, N)
         x = np.cumsum(dx, axis=1)
@@ -59,13 +60,12 @@ class BrownianAnimation():
         dz = np.sqrt(dt) * np.random.randn(1, N)
         z = np.cumsum(dz, axis=1)
 
-        # Creates nested lists, vertically stacking the random variables into numpy arrays for 2D and 3D 
+        ### Creates nested lists, vertically stacking the random variables into numpy arrays for 2D and 3D 
         linedata_1D = np.vstack((pos, x))
-        print(linedata_1D)
         linedata_2D = np.vstack((x, y))
         linedata_3D = np.vstack((x, y, z))
 
-        # Returns an array of shape (dimension, N)
+        ### Returns an array of shape (dimension, N)
         if dimension == '1D':
             return linedata_1D
         elif dimension == '2D':
@@ -93,8 +93,10 @@ class BrownianAnimation():
         for line, data in zip(lines, walkData):
             if dimension == '1D':
                 line.set_data(data[0:2, 0:num])
+
             if dimension == '2D':
                 line.set_data(data[0:2, 0:num])
+
             elif dimension == '3D':   # dimension == '3D'
                 line.set_data(data[0:2, :num])
                 line.set_3d_properties(data[2, 0:num])
@@ -102,18 +104,90 @@ class BrownianAnimation():
         return lines
 
 
-    def animation_1D(self):
-        # Initialise parameters as previously
+    def animate(self, dimension):
+        '''
+        Creates animations in 1D, 2D and 3D, using matplotlib.animation.
+
+        Parameters
+        ----------
+        dimension : str
+            Selects whether the animation is plotted in 1D, 2D or 3D
+        '''
+
+        ### Initialise parameters as previously
         T = 1.0   
         N = 1001 
         dt = math.sqrt(T/(N-1))    
 
-        # Put lineData list into another list (needed for matplotlib.lines.Line2D module)
+        ### Put lineData list into another list (needed for matplotlib.lines.Line2D module)
+        walkData = [self.generate_random_walks(dt, N, dimension)]
+
+        if dimension == '1D':
+            pos = walkData[0][0]
+            x = walkData[0][1]
+
+        elif dimension == '2D':
+            x = walkData[0][0]
+            y = walkData[0][1]
+
+        else:   # dimension == '3D'
+            x = walkData[0][0]
+            y = walkData[0][1]
+            z = walkData[0][2]
+
+        ### Create figure and axis objects with tight axes
+        fig = plt.figure()
+
+        if dimension == '1D':
+            ax = fig.add_subplot(1, 1, 1)
+            ax = plt.axes(xlim=(min(pos)+0.1*min(pos), max(pos)+0.1*max(pos)), ylim=(min(x)+0.1*min(x), max(x)+0.1*max(x)))
+            ax.set_xlabel('Number of steps')
+            ax.set_ylabel('X(t)')
+            ax.set_title('1D Discretised Brownian Path')
+
+        elif dimension == '2D':
+            ax = fig.add_subplot(1, 1, 1)
+            ax = plt.axes(xlim=(min(x)+0.1*min(x), max(x)+0.1*max(x)), ylim=(min(y)+0.1*min(y), max(y)+0.1*max(y)))
+            ax.set_xlabel('X(t)')
+            ax.set_ylabel('Y(t)')
+            ax.set_title('2D Discretised Brownian Paths')
+
+        else:   # dimension == '3D'
+            ax = fig.add_subplot(1, 1, 1, projection='3d')
+            ax.set_xlabel('X(t)')
+            ax.set_xlim3d((min(x)+0.1*min(x), max(x)+0.1*max(x)))
+            ax.set_ylabel('Y(t)')
+            ax.set_ylim3d((min(y)+0.1*min(y), max(y)+0.1*max(y)))
+            ax.set_zlabel('Z(t)')
+            ax.set_zlim3d((min(z)+0.1*min(z), max(z)+0.1*max(z)))
+            ax.set_title('3D Discretised Brownian Paths')
+
+        ### Use list comprehension to create a list of Line2D Objects
+        if dimension == '1D' or '2D':
+            lines = [ax.plot(data[0, 0:1], data[1, 0:1])[0] for data in walkData]
+        
+        else:   # dimension == '3D'
+            lines = [ax.plot(data[0, 0:1], data[1, 0:1], data[2, 0:1])[0] for data in walkData]
+
+        ### Create animation object using the FuncAnimation() method
+        anim = animation.FuncAnimation(
+        fig, self.update_random_walks, N+1, fargs=(walkData, lines, dimension), interval=30, repeat=True, blit=False)
+
+        plt.show()
+
+
+    def animation_1D(self):
+        ### Initialise parameters as previously
+        T = 1.0   
+        N = 1001 
+        dt = math.sqrt(T/(N-1))    
+
+        ### Put lineData list into another list (needed for matplotlib.lines.Line2D module)
         walkData = [self.generate_random_walks(dt, N, '1D')]
         pos = walkData[0][0]
         x = walkData[0][1]
 
-        # Create figure and axis objects with tight axes
+        ### Create figure and axis objects with tight axes
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1)
         ax = plt.axes(xlim=(min(pos)+0.1*min(pos), max(pos)+0.1*max(pos)), ylim=(min(x)+0.1*min(x), max(x)+0.1*max(x)))
@@ -122,10 +196,10 @@ class BrownianAnimation():
         ax.set_ylabel('X(t)')
         ax.set_title('1D Discretised Brownian Path')
 
-        # Use list comprehension to create a list of Line2D Objects
+        ### Use list comprehension to create a list of Line2D Objects
         lines = [ax.plot(data[0, 0:1], data[1, 0:1])[0] for data in walkData]
         
-        # Create animation object using the FuncAnimation() method
+        ### Create animation object using the FuncAnimation() method
         anim = animation.FuncAnimation(
             fig, self.update_random_walks, N+1, fargs=(walkData, lines, '1D'), interval=30, repeat=True)
 
@@ -133,17 +207,17 @@ class BrownianAnimation():
 
 
     def animation_2D(self):
-        # Initialise parameters as previously
+        ### Initialise parameters as previously
         T = 1.0   
         N = 1001 
         dt = math.sqrt(T/(N-1))    
 
-        # Put lineData list into another list (needed for matplotlib.lines.Line2D module)
+        ### Put lineData list into another list (needed for matplotlib.lines.Line2D module)
         walkData = [self.generate_random_walks(dt, N, '2D')]
         x = walkData[0][0]
         y = walkData[0][1]
 
-        # Create figure and axis objects with tight axes
+        ### Create figure and axis objects with tight axes
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1)
         ax = plt.axes(xlim=(min(x)+0.1*min(x), max(x)+0.1*max(x)), ylim=(min(y)+0.1*min(y), max(y)+0.1*max(y)))
@@ -152,10 +226,10 @@ class BrownianAnimation():
         ax.set_ylabel('Y(t)')
         ax.set_title('2D Discretised Brownian Paths')
 
-        # Use list comprehension to create a list of Line2D Objects
+        ### Use list comprehension to create a list of Line2D Objects
         lines = [ax.plot(data[0, 0:1], data[1, 0:1])[0] for data in walkData]
 
-        # Create animation object using the FuncAnimation() method
+        ### Create animation object using the FuncAnimation() method
         anim = animation.FuncAnimation(
             fig, self.update_random_walks, N+1, fargs=(walkData, lines, '2D'), interval=30, repeat=True)
 
@@ -163,18 +237,18 @@ class BrownianAnimation():
 
 
     def animation_3D(self):
-        # Initialise parameters as previously
+        ### Initialise parameters as previously
         T = 1.0   
         N = 1001 
         dt = math.sqrt(T/(N-1))  
 
-        # Put lineData list into another list (needed for matplotlib.lines.Line2D module)
+        ### Put lineData list into another list (needed for matplotlib.lines.Line2D module)
         walkData = [self.generate_random_walks(dt, N, '3D')]
         x = walkData[0][0]
         y = walkData[0][1]
         z = walkData[0][2]
 
-        # Create figure and axis objects with tight axes
+        ### Create figure and axis objects with tight axes
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1, projection='3d')
 
@@ -186,10 +260,10 @@ class BrownianAnimation():
         ax.set_zlim3d((min(z)+0.1*min(z), max(z)+0.1*max(z)))
         ax.set_title('3D Discretised Brownian Paths')
 
-        # Use list comprehension to create a list of Line2D Objects
+        ### Use list comprehension to create a list of Line2D Objects
         lines = [ax.plot(data[0, 0:1], data[1, 0:1], data[2, 0:1])[0] for data in walkData]
 
-        # Create animation object using the FuncAnimation() method
+        ### Create animation object using the FuncAnimation() method
         anim = animation.FuncAnimation(
             fig, self.update_random_walks, N+1, fargs=(walkData, lines, '3D'), interval=30, repeat=True, blit=False)
 
@@ -197,6 +271,6 @@ class BrownianAnimation():
 
 
 if __name__ == '__main__':
-    # Create instance of class and call relevant method
+    ### Create instance of class and call relevant method
     test = BrownianAnimation()
-    test.animation_1D()
+    test.animate('3D')
