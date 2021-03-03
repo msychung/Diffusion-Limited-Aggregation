24b48cac9f1a2b07941633a50e2de31a3460618a
Melissa Chung <melissachung888@gmail.com>
2021-03-03 20:49:05 +0000

    Added class attributes

--- random-processes/random-walks/variablestep_prototype.py
+++ random-processes/random-walks/variablestep_prototype.py
@@ -36,9 +36,14 @@ class BrownianMotion():
         3D Brownian motion for multiple maths, using a vectorised method
     '''
 
-    def __init__(self):
-        pass
-        # Might need to put something in here later on...
+    def __init__(self, T, N, M):
+        np.random.seed(5)
+        self.T = T    # time step
+        self.N = N    # number of steps
+        self.M = M    # number of paths ('walkers')
+        
+        self.dt = math.sqrt(T/(N-1))    # sqrt of time interval
+        self.t = np.linspace(0, T, N)    # Create time list
 
 
     def xy_line(self):
@@ -47,10 +52,8 @@ class BrownianMotion():
         Produces a scatter plot of x co-ordinates against y co-ordinates, following a y = x line with minor deviations. 
         Should produce a slightly different plot each time (because it's random..!), better approximating a straight line y = x for a greater N. 
         '''
-        ### Initialise parameters
-        T = 10.0    # time step
-        N = 101     # number of steps (+1 to give N-1 subintervals)
-        h = math.sqrt(T/N)
+        ### Initialise method parameters
+        h = math.sqrt(self.T/self.N)
 
         '''For larger N, it is more efficient to append to lists then call the dataframe constructor, than to create an empty dataframe and append to it.
         This is because pandas append() returns a copy of the original df and the new one (quadratic copy with complexity O(N^2))'''
@@ -60,7 +63,7 @@ class BrownianMotion():
         y_list = []
 
         ### Loop to fill lists 
-        for i in range(1, N + 1):
+        for i in range(1, self.N + 1):
             '''random() generates a random number from the uniform distribution'''
             x = i + h * random()
             y = i + h * random()
@@ -83,30 +86,23 @@ class BrownianMotion():
         '''
         1D Brownian Motion Path for a single walker, using a for loop
         '''
-        ### Initialise parameters
-        T = 1.0    # time step
-        N = 10001     # number of steps (+1 to give N-1 subintervals)
-        M = 10      # number of paths ('walkers')
-        dt = math.sqrt(T/(N-1))    # sqrt of time interval (one second/10001 steps)
-        t = np.linspace(0, T, N)    # Create time list
-
         ### Create lists of zeroes of length N
-        x = [0] * N    # Random variable x
-        dx = [0] * N     # Increment dx
+        x = [0] * self.N    # Random variable x
+        dx = [0] * self.N     # Increment dx
 
         ### Set initial values 
-        dx[0] = dt * np.random.randn()  # multiples time interval by some random number between -1 and 1
+        dx[0] = self.dt * np.random.randn()  # multiples time interval by some random number in the SNN
         x[0] = dx[0]
 
         ### Loop to fill rest of the elements of the lists
-        for i in range (1, N):
+        for i in range (1, self.N):
             '''np.random.randn() generates a random number from the standard normal distribution'''
-            dx[i] = dt * np.random.normal()
+            dx[i] = self.dt * np.random.normal()
             x[i] = x[i-1] + dx[i] 
 
         ### Create dataframe and fill with lists, then delete lists
-        df = pd.DataFrame({'t': t, 'x': x, 'dx': dx})
-        del t, x, dx
+        df = pd.DataFrame({'t': self.t, 'x': x, 'dx': dx})
+        del x, dx
 
         ### Plot t against x 
         fig, ax = plt.figure(), plt.axes()
@@ -119,28 +115,21 @@ class BrownianMotion():
         '''
         1D Brownian Motion Path, using vectorised method and for multiple (M) walkers
         '''
-        ### Initialise parameters
-        T = 1.0    # time step
-        N = 1001     # number of steps (+1 to give N-1 subintervals)
-        M = 10    # number of paths ('walkers')
-        dt = math.sqrt(T/(N-1))    # sqrt of time interval
-        t = np.linspace(0, T, N)    # Create time list
-
-        ### Vectorised method for multiple paths
-        dx = dt * np.random.randn(M, N)
+        ### Vectorised method for multiple paths (MxN array)
+        dx = self.dt * np.random.randn(self.M, self.N)
         x = np.cumsum(dx, axis = 1)
         
         ### Assign values into a dataframe
         df = pd.DataFrame(x).T      # takes transpose to flip rows/columns
-        df.insert(0, 'Time', t, True)
+        df.insert(0, 'Time', self.t, True)
 
         ### Plot t against x 
         fig, ax = plt.figure(), plt.axes()
 
         # Aesthetics: cycles through a colormap
-        ax.set_prop_cycle('color', plt.cm.winter(np.linspace(0, 4, M*4)))  
+        ax.set_prop_cycle('color', plt.cm.winter(np.linspace(0, 4, self.M*4)))  
 
-        for i in range(M):
+        for i in range(self.M):
             ax.plot(df['Time'], df.iloc[:, i+1], marker='o', markersize=0.25, linewidth=0)
 
         ax.set(xlabel='Time t', ylabel='Random Variable $X(t)$', title='1D Brownian Motion Multiple Paths')
@@ -152,46 +141,39 @@ class BrownianMotion():
         '''
         2D Brownian Motion Path for a single walker, using a for loop
         '''
-        ### Initialise parameters
-        T = 1.0    # time step
-        N = 1001     # number of steps (+1 to give N-1 subintervals)
-        M = 10      # number of paths ('walkers')
-        dt = math.sqrt(T/(N-1))    # sqrt of time interval
-        t = np.linspace(0, T, N)    # Create time list
-
         ### Create lists of zeroes of length N
-        x = [0] * N    # Random variable x
-        dx = [0] * N     # Increment dx
+        x = [0] * self.N    # Random variable x
+        dx = [0] * self.N     # Increment dx
 
-        y = [0] * N    # Random variable y
-        dy = [0] * N    # Increment dx
+        y = [0] * self.N    # Random variable y
+        dy = [0] * self.N    # Increment dx
 
         ### Set initial values 
-        dx[0] = dt * np.random.randn()
+        dx[0] = self.dt * np.random.randn()
         x[0] = dx[0]
 
-        dy[0] = dt * np.random.randn()
+        dy[0] = self.dt * np.random.randn()
         y[0] = dy[0]
 
         ### Loop to fill rest of the elements of the lists
-        for i in range (1, N):
+        for i in range (1, self.N):
             '''np.random.randn() generates a random number from the standard normal distribution'''
-            dx[i] = dt * np.random.normal()
+            dx[i] = self.dt * np.random.normal()
             x[i] = x[i-1] + dx[i] 
 
-            dy[i] = dt * np.random.normal()
+            dy[i] = self.dt * np.random.normal()
             y[i] = y[i-1] + dy[i] 
 
         ### Create dataframe and fill with lists, then delete lists
-        df = pd.DataFrame({'t': t, 'x': x, 'dx': dx, 'y': y, 'dy': dy})
-        del t, x, dx, y, dy
+        df = pd.DataFrame({'t': self.t, 'x': x, 'dx': dx, 'y': y, 'dy': dy})
+        del x, dx, y, dy
 
         ### Plot x against y
         fig = plt.figure()
         ax = fig.add_subplot(111)
 
         # Aesthetics: cycles through a colormap
-        ax.set_prop_cycle('color', plt.cm.winter(np.linspace(0, 4, 45)))  
+        ax.set_prop_cycle('color', plt.cm.winter(np.linspace(0, 4, self.M*4)))  
 
         # for i in range(M):
         ax.plot(df['x'], df['y'], marker='o', markersize=1, linewidth=0.5)
@@ -207,34 +189,27 @@ class BrownianMotion():
         '''
         2D Brownian Motion Path, using vectorised method and for multiple (M) walkers
         '''
-        ### Initialise parameters
-        T = 1.0    # time step
-        N = 101     # number of steps (+1 to give N-1 subintervals)
-        M = 6    # number of paths ('walkers')
-        dt = math.sqrt(T/(N-1))    # sqrt of time interval
-        t = np.linspace(0, T, N)    # Create time list
-
-        ### Vectorised method for multiple paths
-        dx = dt * np.random.randn(M, N)
+        ### Vectorised method for multiple paths (MxN array)
+        dx = self.dt * np.random.randn(self.M, self.N)
         x = np.cumsum(dx, axis = 1)
 
-        dy = dt * np.random.randn(M, N)
+        dy = self.dt * np.random.randn(self.M, self.N)
         y = np.cumsum(dy, axis = 1)
         
         ### Assign values into a dataframe
         df_x = pd.DataFrame(x).T
         df_y = pd.DataFrame(y).T
         df_join = pd.concat([df_x, df_y], axis = 1)
-        df_join.insert(0, 'Time', t, True)
+        df_join.insert(0, 'Time', self.t, True)
 
         ### Plot x against y 
         fig, ax = plt.figure(), plt.axes()
 
         # Aesthetics: cycles through a colormap
-        ax.set_prop_cycle('color', plt.cm.winter(np.linspace(0, 4, M*4)))  
+        ax.set_prop_cycle('color', plt.cm.winter(np.linspace(0, 4, self.M*4)))  
 
-        for i in range(M):
-            ax.plot(df_join.iloc[:, i+1], df_join.iloc[:, i+1+M], marker='o', markersize=0.25, linewidth=0.2)
+        for i in range(self.M):
+            ax.plot(df_join.iloc[:, i+1], df_join.iloc[:, i+1+self.M], marker='o', markersize=0.25, linewidth=0.2)
 
         ax.set(xlabel='Random Variable $X(t)$', ylabel='Random Variable $Y(t)$', title='2D Brownian Motion Multiple Paths')
 
@@ -245,21 +220,14 @@ class BrownianMotion():
         '''
         3D Brownian Motion Path, using vectorised method and for multiple (M) walkers
         '''
-        ### Initialise parameters
-        T = 1.0    # time step
-        N = 101     # number of steps (+1 to give N-1 subintervals)
-        M = 6    # number of paths ('walkers')
-        dt = math.sqrt(T/(N-1))    # sqrt of time interval
-        t = np.linspace(0, T, N)    # Create time list
-
-        ### Vectorised method for multiple paths
-        dx = dt * np.random.randn(M, N)
+        ### Vectorised method for multiple paths (MxN array)
+        dx = self.dt * np.random.randn(self.M, self.N)
         x = np.cumsum(dx, axis = 1)
 
-        dy = dt * np.random.randn(M, N)
+        dy = self.dt * np.random.randn(self.M, self.N)
         y = np.cumsum(dy, axis = 1)
 
-        dz = dt * np.random.randn(M, N)
+        dz = self.dt * np.random.randn(self.M, self.N)
         z = np.cumsum(dz, axis = 1)
         
         ### Assign values into a dataframe
@@ -267,17 +235,17 @@ class BrownianMotion():
         df_y = pd.DataFrame(y).T
         df_z = pd.DataFrame(z).T
         df_join = pd.concat([df_x, df_y, df_z], axis = 1)
-        df_join.insert(0, 'Time', t, True)
+        df_join.insert(0, 'Time', self.t, True)
 
         ### Plot t against x 
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1, projection='3d')
 
         # Aesthetics: cycles through a colormap
-        ax.set_prop_cycle('color', plt.cm.winter(np.linspace(0, 4, M*4)))  
+        ax.set_prop_cycle('color', plt.cm.winter(np.linspace(0, 4, self.M*4)))  
 
-        for i in range(M):
-            ax.plot(df_join.iloc[:, i+1], df_join.iloc[:, i+1+M], df_join.iloc[:, i+1+M+M], marker='o', markersize=0.25, linewidth=0.2)
+        for i in range(self.M):
+            ax.plot(df_join.iloc[:, i+1], df_join.iloc[:, i+1+self.M], df_join.iloc[:, i+1+self.M+self.M], marker='o', markersize=0.25, linewidth=0.2)
 
         ax.set(xlabel='Random Variable $X(t)$', ylabel='Random Variable $Y(t)$', zlabel='Random Variable $Z(t)$', title='3D Brownian Motion Multiple Paths')
 
@@ -286,5 +254,5 @@ class BrownianMotion():
 
 if __name__ == '__main__':
     ### Create instance of class and call relevant method
-    test = BrownianMotion()
+    test = BrownianMotion(1.0, 1000, 5)
     test.brownian_3D_vec()
