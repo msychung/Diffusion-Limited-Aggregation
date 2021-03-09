32417359ed96c938ba9bee671d09ba9e6bec6fe6
Melissa Chung <melissachung888@gmail.com>
2021-03-09 14:51:27 +0000

    Particles stick!!! 8 directions instead of 4

--- DLA/dla_prototype.py
+++ DLA/dla_prototype.py
@@ -3,11 +3,13 @@ import numpy as np
 import pygame
 
 class Particle():
-    
+
     def __init__(self, width, height):
         # Set starting co-ordinates
         self.x = round(width/2)
-        self.y = round(height/2)  
+        self.y = round(height/2) 
+
+        self.stick= False 
 
 
 class Application():
@@ -17,15 +19,11 @@ class Application():
         self.displaySurface = None
         self.size = self.width, self.height = 962, 601      # size of display screen
         self.pixelArray = None
-        self.pixelColor = None
+        self.crystalColor = 0xDCDCDC
         
         # Set starting co-ordinates
         self.start_x = round(self.width/2)
         self.start_y = round(self.height/2)  
-
-        # # Create x and y variables to store co-ordinates
-        # self.x = self.start_x
-        # self.y = self.start_y 
 
         self.updateFlag = False
 
@@ -33,7 +31,14 @@ class Application():
         for i in range(n):
             particle = Particle(self.width, self.height)   #  yaaayy composition! 
             self.all_particles.append(particle)
+
+        self.crystal_position = []
         
+        self.padSize = 30
+        self.domainMin_x = self.start_x - self.padSize
+        self.domainMax_x = self.start_x + self.padSize
+        self.domainMin_y = self.start_y - self.padSize
+        self.domainMax_y = self.start_y + self.padSize
 
     def on_init(self):
         pygame.init()
@@ -41,12 +46,8 @@ class Application():
 
         self.displaySurface = pygame.display.set_mode(self.size)      # Create display surface
         self.pixelArray = pygame.PixelArray(self.displaySurface)      # Create pixel array
-        self.pixelColor = (220, 220, 220)                             # Set pixel colour in RGB
-        self.isRunning = True
 
-        ### Create a seed point
-        # self.pixelArray[self.start_x - 50, self.start_y - 50] = 0xDCDCDC    # Must be same colour as other particles otherwise they won't stick!
-        # pygame.display.update()
+        self.isRunning = True
     
 
     def on_event(self, event):
@@ -62,11 +63,14 @@ class Application():
         '''
         Adds one pixel to each random walk
         '''
-        
-        ss = 1    # set step size
+        ss = 2    # set step size
 
         for particle in self.all_particles:
-            (dx, dy) = random.choice([(0, ss), (0, -ss), (ss, 0), (-ss, 0)])
+            if particle.stick:
+                continue
+
+            # (dx, dy) = random.choice([(0, ss), (0, -ss), (ss, 0), (-ss, 0)])
+            (dx, dy) = random.choice([(0, ss), (0, -ss), (ss, 0), (-ss, 0), (ss, -ss), (-ss, ss), (ss, ss), (-ss,-ss)])
 
             ### Assign to new x and y variables to keep a record of current and future position
             new_x = particle.x + dx
@@ -83,40 +87,40 @@ class Application():
                 new_y = self.height - 1
 
             ### Check if pixel has already been covered by walker (need to use hex colour codes)
-            if self.pixelArray[new_x, new_y] == 0xDCDCDC:  # light gray
+            if self.pixelArray[new_x, new_y] == self.crystalColor:  # light gray
                 self.updateFlag = True
 
             else:
                 self.updateFlag = False
                 particle.x, particle.y = new_x, new_y
 
-            self.on_render(particle)
+            self.on_render(particle)    # call on_render here to be able to pass in particle attributes x, y
 
         pygame.display.update()
-        self.displaySurface.fill((0,0,0,))
+        self.displaySurface.fill((0,0,0,))      # Removes previous path of particles
 
 
     def on_render(self, particle):
         '''
-        Updates the pixel array IF a particle is allocated to the growing crystal
+        Updates the pixel array if a particle is allocated to the growing crystal, otherwise shows movements of particles on their random walks
         '''
-        ## Create seed
-        self.pixelArray[self.start_x - 10, self.start_y - 10] = 0xDCDCDC    # Must be same colour as other particles otherwise they won't stick!
-        pygame.display.update()
+        ## Create seed - must be same colour as other particles or they won't stick!
+        self.pixelArray[self.start_x - 100, self.start_y - 100] = self.crystalColor
 
+        ### Recolour crystal each time
+        for coordinate in self.crystal_position:
+            self.pixelArray[coordinate[0], coordinate[1]] = self.crystalColor
+
+        ### If particle sticks, set its pixel to grey and particle.stick = True stops it from moving any further
         if self.updateFlag:  
-            self.pixelArray[particle.x, particle.y] = 0xDCDCDC
-
-            # Update the display
-            pygame.display.update()
+            self.crystal_position.append((particle.x, particle.y))
 
             # Reset update flag and x, y co-ordinates to restart random walk
             self.updateFlag = False
-            particle.x, particle.y = self.start_x, self.start_y
+            particle.stick = True
 
         if not self.updateFlag:
-            self.pixelArray[particle.x, particle.y] = 0x00FF00
-            pygame.display.update()
+            self.pixelArray[particle.x, particle.y] = 0x00FF00   # green
 
 
     def on_execute(self):
@@ -136,6 +140,7 @@ class Application():
 
         pygame.quit()
 
+
 if __name__ == '__main__':
-    test = Application(50)
+    test = Application(500)
     test.on_execute()
