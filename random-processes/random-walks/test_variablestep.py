59ed238d43d1aaa2ffadb036e4e175a25fb6fee9
Melissa Chung <melissachung888@gmail.com>
2021-03-26 03:06:09 +0000

    Added fixture and function tests

--- random-processes/random-walks/test_variablestep.py
+++ random-processes/random-walks/test_variablestep.py
@@ -1,1 +1,37 @@
-### Test
+from variablestep import Variable_Step
+import pytest
+
+@pytest.fixture
+def walk():
+    T = 100
+    N = 5
+    M = 3
+    iterations = 10
+    return Variable_Step(T, N, M, iterations)
+
+def test_variable_step_init(walk):
+    assert walk.T == 100
+    assert walk.N == 5
+    assert walk.M == 3
+    assert walk.iterations == 10
+    assert round(walk.dt) == 5
+    assert len(walk.t) == 5
+
+def test_brownian_1D_loop(walk):
+    assert len(walk.brownian_1D_loop()['x']) == 5
+
+def test_brownian_1D_vec(walk):
+    assert len(walk.brownian_1D_vec()['Time']) == 5
+
+def test_brownian_2D_loop(walk):
+    assert len(walk.brownian_2D_loop()['x']) == 5
+
+def test_brownian_2D_vec(walk):
+    assert len(walk.brownian_2D_vec()['Time']) == 5
+
+def test_brownian_3D_vec(walk):
+    assert len(walk.brownian_3D_vec()['Time']) == 5
+
+def test_calc_displacements(walk):
+    assert round(walk.calc_displacements('1D'), 1) == 2.4
+    assert round(walk.calc_displacements('2D'), 1) == 2.1
