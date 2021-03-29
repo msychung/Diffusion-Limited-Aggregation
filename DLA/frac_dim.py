35a99b3b4aa93ef52c6f79c6aa8935d44b530ad7
Melissa Chung <melissachung888@gmail.com>
2021-03-29 07:36:40 +0100

    Added documentation

--- DLA/frac_dim.py
+++ DLA/frac_dim.py
@@ -5,14 +5,15 @@ from dla_prototype import Application
 import matplotlib.pyplot as plt
 plt.style.use('seaborn-whitegrid')
 
+
 class Fractal_Dimension():
     '''
-    Calculates the Hausdorff dimension H_d of a 2D DLA cluster and plots a log-log mass vs. radius graph 
+    Calculates the log(mass) and log(radius) of a 2D DLA cluster in order to calculate Hausdorff dimension H_d.
     '''
 
     def __init__(self):
         '''
-        Adjust parameters accordingly and set range of for loop to determine number of iterations
+        Adjust parameters accordingly and sets a range of for loop to determine number of iterations.
         '''
         self.clusters = [Application(200, 'dot', 'circle', 50, 40, run) for run in range(2, 180, 2)]
         
@@ -20,10 +21,6 @@ class Fractal_Dimension():
     def cluster_mass(self):
         '''
         Calculates the 'mass' of the DLA cluster by counting the number of particles (pixels). 
-
-        Parameters
-        ----------
-        None
 
         Returns
         -------
@@ -33,11 +30,13 @@ class Fractal_Dimension():
         logMass_list : list
             List containing the ln(value) of each element in mass_list
         '''
+        ### Create empty lists to store values
         mass_list = []
         logMass_list = []
-        time_list = []
 
+        ### Loop through all simulation iterations
         for cluster in self.clusters:
+            # Find the length of the crystal_position list (containing all pixels forming the crystal)
             mass = len(cluster.crystal_position)
             mass_list.append(mass)
 
@@ -51,10 +50,6 @@ class Fractal_Dimension():
         '''
         Calculates the 'radius of gyration' of the DLA cluster based on a list of pixel positions
 
-        Parameters
-        ----------
-        None
-
         Returns
         -------
         max_radius_list : list
@@ -63,9 +58,11 @@ class Fractal_Dimension():
         logRadius_list: list
             List containing the ln(value) of each element in max_radius_list
         '''
+        ### Create empty lists to store values
         max_radius_list = []
         logRadius_list = []
 
+        ### Loop through all simulation iterations
         for cluster in self.clusters:
             max_radius = 0
             for pixel in cluster.crystal_position:
@@ -87,38 +84,44 @@ def print_save_results(test):
     '''
     Prints the lists of interests (optional) and creates a pandas DataFrame to store and save lists.
     '''
+    ### Call functions to obtain lists
     mass_list, logMass_list = test.cluster_mass()
     max_radius_list, logRadius_list = test.cluster_radius()
+
+    ### Optionally print individual lists (uncomment)
     # print("List of N: ", mass_list)
     # print("List of ln(mass): ", logMass_list)
     # print("List of crystal radii: ", max_radius_list)
     # print("List of ln(radius): ", logRadius_list)
 
+    ### Create a pandas DataFrame using above lists and save the stored values
     df = pd.DataFrame({'mass': mass_list, 'radius': max_radius_list, 'ln(mass)': logMass_list, 'ln(radius)': logRadius_list})
     # df.to_pickle("Fractal Dimension Circle")
 
-    print(df.head)
+    print(df.head)     # Print the first few rows of the DataFrame
 
 
 def plot_frac_dim():
     '''
-    Plots the relevant columns of the pandas DataFrame along with the best linear fit to data
-
-    Parameters
-    ----------
-    None
+    Plots the relevant columns of the pandas DataFrame into a log-log mass vs. radius graph, along with the best linear fit to data.
 
     Returns
     -------
     slope : float
         The slope of the log-log plot, representing the value of the Hausdorff dimension H_d
     '''
+    ### Read in the pandas DataFrame
     df = pd.read_pickle("Fractal Dimension Square")
+
+    ### Editing one of the dataframes because my laptop overheated and the last few results were wrong
     logRadius_list = np.log(df['radius'])
     df['ln(radius)2'] = logRadius_list
     df = df.iloc[:-25]
 
-    slope, intercept = np.polyfit(df['ln(radius)2'], df['ln(mass)'], 1)  # Create a least squares polynomial fit 
+    print(df)
+
+    # Create a least squares polynomial fit 
+    slope, intercept = np.polyfit(df['ln(radius)2'], df['ln(mass)'], 1)  
     slope_func = np.poly1d(slope)
 
     ### Plot ln(mass) against ln(radius)
@@ -128,7 +131,7 @@ def plot_frac_dim():
     ax.plot(df['ln(radius)2'], df['ln(mass)'], marker='o', markersize=1.5, linewidth=0)
     ax.plot(df['ln(radius)2'], slope*df['ln(radius)2'] + intercept, color='dodgerblue', linewidth=0.5)
     
-    ax.set_xlabel('ln(radius)')
+    ax.set_xlabel('ln(radius)2')
     ax.set_ylabel('ln(mass)')
     ax.title.set_text('Log-log plot of DLA crystal mass against radius')
 
@@ -144,5 +147,5 @@ if __name__ == '__main__':
     # df = pd.read_pickle("Fractal Dimension Circle")
     # print(df.head)
 
-    # slope = plot_frac_dim()
-    # print(slope)
+    slope = plot_frac_dim()
+    print(slope)
