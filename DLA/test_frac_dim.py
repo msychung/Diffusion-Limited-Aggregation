e3a9acbf58474bfab59bc1b2267ba5623059d4d0
Melissa Chung <melissachung888@gmail.com>
2021-03-29 14:21:59 +0100

    Completed tests

--- DLA/test_frac_dim.py
+++ DLA/test_frac_dim.py
@@ -1,1 +1,21 @@
-### Test
+import pytest
+from frac_dim import Fractal_Dimension
+
+@pytest.fixture
+def fractal():
+    n = 100
+    seed_shape = 'dot'
+    spawn_shape = 'square'
+    padSize = 50
+    return Fractal_Dimension(n, seed_shape, spawn_shape, padSize)
+
+def test_fractal_dimension_init(fractal):
+    assert len(fractal.clusters) == 89
+
+def test_cluster_mass(fractal):
+    assert len(fractal.cluster_mass()[0]) == 89
+    assert len(fractal.cluster_mass()[1]) == 89
+
+def test_cluster_radius(fractal):
+    assert len(fractal.cluster_radius()[0]) == 89
+    assert len(fractal.cluster_radius()[1]) == 89
